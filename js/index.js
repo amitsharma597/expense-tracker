2b8acc5dcf8b6eb137a5b088c2e34b4740f15682
amitsharma597 <amitsharmahsp508@gmail.com>
2026-02-13 19:17:56 +0530

    add js logic to delete expenses

--- js/index.js
+++ js/index.js
@@ -3,25 +3,52 @@ document.addEventListener("DOMContentLoaded", () => {
     const description = document.getElementById('input-description');
     const expenseList = document.getElementById('expense-list');
     const addListBtn = document.getElementById('add-list-btn');
+    const deleteBtn = document.getElementsByClassName('delete-btn');
+
 
     const showList = () => {
-           const li = document.createElement('li');
-           li.innerHTML = 
-            `<span>${amount.value}</span>
-           <span>${description.value}</span> `
-           ;
-           expenseList.appendChild(li);
 
-           amount.value = '';
-           description.value = '';
+    if(amount.value.trim()===""){
+        return;
+    }
+        const li = document.createElement('li');
+        li.innerHTML =
+            `<span>${amount.value.trim()}</span>
+           <span>${description.value.trim()}</span>
+         <button class = "delete-btn" type="button">delete</button>`;
+
+        expenseList.appendChild(li);
+
+        amount.value = '';
+        description.value = '';
+
+        deleteBtn.addEventListener('click', ()=>{
+            expenseList.removeChild(li);
+        })
 
 
-        };
-    
-    
-    addListBtn.addEventListener('click', showList );
+    };
 
- 
+    addListBtn.addEventListener('click', showList);
+
+    amount.addEventListener('keypress', (e) => {
+        if (e.key == "Enter") {
+            showList(e);
+        }
+    });
+    description.addEventListener('keypress', (e) => {
+        if (e.key == "Enter") {
+            showList(e);
+        }
+    });
+
+
+
+
+
+
+
+
 
 
 
